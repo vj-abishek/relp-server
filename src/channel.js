f1e7638e982ea1139fafdf8f712a48c8d378563d
vj-abishek <abigosearch@gmail.com>
2020-10-10 12:47:10 +0530

    Commit

--- src/channel.js
+++ src/channel.js
@@ -1,45 +1,45 @@
 module.exports = (server) => {
-  const socketIO = require("socket.io");
-  const Message = require("./model/Message.model");
-  const User = require("./model/User.model");
-  const Token = require("./model/Token.model");
-  const { notify, UserSocketId } = require("./Utils");
+  const socketIO = require('socket.io');
+  const Message = require('./model/Message.model');
+  const User = require('./model/User.model');
+  const Token = require('./model/Token.model');
+  const { notify, UserSocketId } = require('./Utils');
   const io = socketIO(server);
   const users = new Map();
 
-  io.on("connection", (socket) => {
-    console.log("Made connection id:", socket.id);
+  io.on('connection', (socket) => {
+    console.log('Made connection id:', socket.id);
 
     // Add to public channel
-    socket.join("anonymous");
+    socket.join('anonymous');
 
-    socket.on("authenticated", async (u) => {
-      socket.leave("anonymous");
-      socket.join("authenticated");
+    socket.on('authenticated', async (u) => {
+      socket.leave('anonymous');
+      socket.join('authenticated');
 
       users.set(u.uid, { id: socket.id, uid: u.uid });
 
       if (u) {
         console.log(u);
-        io.emit("user status", {
+        io.emit('user status', {
           uid: u.uid,
           LastSeen: Date.now(),
-          status: "Online",
+          status: 'Online',
         });
         // Add the user status in the database
         const dbUser = await User.exists({ uid: u.uid });
         if (dbUser) {
           await User.where({ uid: u.uid }).updateOne({
             LastSeen: Date.now(),
-            status: "Online",
+            status: 'Online',
           });
         } else {
           const newUser = new User({
             uid: u.uid,
             LastSeen: Date.now(),
-            status: "Online",
+            status: 'Online',
           });
-          newUser.save().then(() => console.log("Added new user"));
+          newUser.save().then(() => console.log('Added new user'));
         }
       }
 
@@ -48,99 +48,99 @@ module.exports = (server) => {
       const message = await Message.find({ to: u.uid });
       console.log(message);
       if (message) {
-        io.to(socket.id).emit("new message", message);
+        io.to(socket.id).emit('new message', message);
       }
       console.log(users);
     });
 
-    socket.on("send message", (message) => {
+    socket.on('send message', (message) => {
       console.log(message);
       const user = UserSocketId(users, message.to);
       if (!user) {
         const res = new Message({ ...message });
-        res.save().then(() => console.log("Saved Successfully"));
+        res.save().then(() => console.log('Saved Successfully'));
         notify(message);
       } else {
-        io.to(user.id).emit("recieve message", message);
+        io.to(user.id).emit('recieve message', message);
         notify(message);
       }
     });
 
-    socket.on("message recieved", async (message) => {
+    socket.on('message recieved', async (message) => {
       await Message.deleteOne({ _id: message._id });
     });
 
-    socket.on("fetch status", async (data) => {
+    socket.on('fetch status', async (data) => {
       const res = await User.find({ uid: data.id });
       if (res) {
-        io.to(socket.id).emit("user status disk", res);
+        io.to(socket.id).emit('user status disk', res);
       }
     });
 
     // Store notification token
-    socket.on("notification token", (data) => {
+    socket.on('notification token', (data) => {
       const token = new Token(data);
       token
         .save()
-        .then(() => console.log("Saved Token "))
+        .then(() => console.log('Saved Token '))
         .catch(console.error);
     });
 
-    socket.on("Typing Indicator", (status) => {
+    socket.on('Typing Indicator', (status) => {
       const user = UserSocketId(users, status.to);
       if (user) {
-        io.to(user.id).emit("Typing Indicator", status);
+        io.to(user.id).emit('Typing Indicator', status);
       }
     });
 
-    socket.on("offer", ({ from, to, payload }) => {
+    socket.on('offer', ({ from, to, payload }) => {
       console.log({ to, payload });
       const user = UserSocketId(users, to);
-      io.to(user.id).emit("backOffer", { from, to, payload });
+      io.to(user.id).emit('backOffer', { from, to, payload });
     });
 
-    socket.on("answer", ({ from, to, payload }) => {
+    socket.on('answer', ({ from, to, payload }) => {
       console.log({ from, to, payload });
       const user = UserSocketId(users, from);
-      io.to(user.id).emit("backAnswer", { from, to, payload });
+      io.to(user.id).emit('backAnswer', { from, to, payload });
     });
 
-    socket.on("shareID", ({ shareID, finalTo }) => {
+    socket.on('shareID', ({ shareID, finalTo }) => {
       console.log(shareID, finalTo);
       const user = UserSocketId(users, finalTo);
-      io.to(user.id).emit("shareID", shareID);
+      io.to(user.id).emit('shareID', shareID);
     });
 
-    socket.on("current channel", (data) => {
+    socket.on('current channel', (data) => {
       const user = UserSocketId(users, data.to);
       if (user !== undefined) {
-        io.to(user.id).emit("current channel", data);
+        io.to(user.id).emit('current channel', data);
       }
     });
 
     // Handle Clean up
-    socket.on("disconnect", async () => {
+    socket.on('disconnect', async () => {
       const values = users.values();
 
       for (const v of values) {
         if (v.id === socket.id) {
           users.delete(v.id);
-          io.emit("user status", {
+          io.emit('user status', {
             LastSeen: Date.now(),
-            status: "Offline",
+            status: 'Offline',
             uid: v.uid,
           });
 
           // Update the user status in the database
           await User.where({ uid: v.uid }).updateOne({
             LastSeen: Date.now(),
-            status: "Offline",
+            status: 'Offline',
           });
           return;
         }
       }
 
-      console.log("Disconnected user id:", socket.id);
+      console.log('Disconnected user id:', socket.id);
     });
   });
 };
